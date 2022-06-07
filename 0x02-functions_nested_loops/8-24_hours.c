858c9cab4b64133b498df58afad0eb48603644d2
serena-Muriuki <serenamuriuki@gmail.com>
2022-06-07 16:56:48 -0700

    Functions and nested loops

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -3,6 +3,8 @@
 /**
  * jack_bauer - Prints every minute of the day of 
  *              Jack Bauer, starting from 00:00 to 23:59.
+ *
+ * Return :Always 0.
  */
 void jack_bauer(void)
 {
@@ -20,4 +22,5 @@ void jack_bauer(void)
 			_putchar('\n');
 		}
 	}
+	return (0);
 }
