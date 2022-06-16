37e6899f52ec78a0ff61f8029d1272b875147996
serena-Muriuki <serenamuriuki@gmail.com>
2022-06-16 09:38:30 -0700

    More arrays

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,13 +9,6 @@
  */
 char *strcat(char *dest, char *src)
 {
-	int index = 0, dest_len = 0;
-
-	while (dest[index++])
-		dest_len++;
-
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
-
+	strcat(dest, src);
 	return (dest);
 }
