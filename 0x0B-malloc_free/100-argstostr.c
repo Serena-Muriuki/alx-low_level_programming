5783893723f7e447e328a84a97b625cf5174a4ef
serena-Muriuki <serenamuriuki@gmail.com>
2022-06-28 08:24:35 -0700

    Malloc

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,38 @@
+#include"main.h"
+#include <stdlib.h>
+
+/**
+ * *argstostr - convert arguments in command line to strings
+ * @ac: int type.
+ * @av: pointer to array
+ * Return: arguments as strings
+ */
+char *argstostr(int ac, char **av)
+{
+	char *str;
+	int arg, byte, index, size = ac;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (arg = 0; arg < ac; arg++)
+	{
+		for (byte = 0; av[arg][byte]; byte++)
+			size++;
+	}
+	str = malloc(sizeof(char) * size + 1);
+
+	if (str == NULL)
+		return (NULL);
+
+	index = 0;
+
+	for (arg = 0; arg < ac; arg++)
+	{
+		for (byte = 0; av[arg][byte]; byte++)
+			str[index++] = av[arg][byte];
+
+		str[index++] = '\n';
+	}
+	str[size] = '\0';
+	return (str);
+}
